cd592733e15da4c61dc29574538f13236b043c00
Jose Carlos <cuentatrabajo2605@gmail.com>
2025-05-15 14:19:29 -0500

    Cambio 1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 setTimeout(() => {
   const confirmar = confirm("¿Quieres completar el formulario y registrarte para disfrutar los beneficios de DON LIMÓN?");
   if (confirmar) {
-    window.location.href = "https://docs.google.com/forms/d/e/tu_formulario_id/viewform"; // Reemplaza con tu URL
+    window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLScbi6Yvm_FYCHGXEDE40N8ejmK0H7z7aDpiO-W7DXcHatrvNA/viewform?usp=header"; // Reemplaza con tu URL
   }
 }, 3000);
